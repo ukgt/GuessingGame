c83975958266b67ce24faf94c6ae9af3e4f1eba8
ukgt <ukgt2019@gmail.com>
2018-12-04 13:36:55 -0500

    updated files

--- assets/game.js
+++ assets/game.js
@@ -20,22 +20,32 @@ var correctGuess =[];          //input letter
 var lettersInWord = [];                           
 var wrongGuess;                //counter will count down by 1
 var remainingGuesses;           //display how many guesses are left
-//var gameOver;               //you win image or game over image
+var gameOver;               //you win image or game over image
 var gameStart;          //press the button to get started
 var resetGame;
 var wins;
 var counter = 10;
 
+// to start game - click on startBtn
+//computer will select a word from wordList to place in _ _ _ _ in currentWord space
+//correctGuess will add letter to _ _ _ _ blanks
+//wrongGuess will make counter count down by 1 and which is shown in Guesses Remaining
+// and also log to guessedLetters
+//if user guesses word in 10 tries, then win count goes up by 1
+// if not, then game resets
 
-function gameStart() {
+
+function startGame (); {
 
     currentWord = wordList[Math.floor(Math.random() * wordList.length)];
     lettersInWord = currentWord.split("");
-    
+   
 };
-gameStart()
-console.log("this is my current word" + currentWord)
-console.log("this is my letter in word" +  lettersInWord)
+startGame()
+console.log("this is my letter in word" +  lettersInWord);
+document.onkeyup("#startBtn"); ("this is my current word" + currentWord);
+
+
 
 
 for (remainingGuesses; counter.length < 10; resetGame){
@@ -62,19 +72,18 @@ console.log("all you used up guesses" + counter)
 
 
 
-/*if (guessedLetters === correctGuess[i]) {
-    let letter = document.createElement ('p');
-        letter.textContent = guessedLetters[i];
-        guessedLetters.appendChild(letter);
-
+if (guessedLetters === correctGuess[i]) {
+    let lettersInWord = document.createElement ('p');
+        lettersInWord.textContent = guessedLetters[i];
+        guessedLetters.appendChild(lettersInWord);
 } 
 
 else {
     guessedLetters.push(guessedLetters);
     console.log(guessedLetters);
-}*/
+}
 
-/*document.onkeyup = function (event) {
+document.onkeyup = function (event) {
     keyPressed = event.key.toLowerCase();
     for (var i; i < guessedLetters.length; i++) {
         if (keyPressed === guessedLetters[i]) {
@@ -86,15 +95,6 @@ else {
 
 
     }
-}*/
-
-
-//for (lettersInWord; correctGuess; ){}
-
-
-
-
-
-//if (guessedLetters[i] === currentWord[i])
+}
 
 
